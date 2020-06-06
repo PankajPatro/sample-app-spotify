075cc863e0d78be40e5840cf43b5d03bace9dc77
Pankaj Patro <patro.pankaj@gmail.com>
2020-06-07 07:25:55 +0800

    removed package-lock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatToolbarModule } from  '@angular/material/toolbar';
-import { MatCardModule } from  '@angular/material/card';
-import { MatButtonModule } from  '@angular/material/button';
-import { MatIconModule } from  '@angular/material/icon';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
